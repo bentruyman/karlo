58953b9d1543449777c7359e00b3fc442658a405
Ben Truyman <ben@truyman.com>
2021-12-25 17:50:56 -0800

    feat(ui): integrate tauri for native executable bundling
    Also converted from Next.js to Parcel as it integrates more
    seamlessly with Tauri.
    
    Closes #15

--- packages/ui/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const Index: NextPage = () => {
-  return <div>Hello World</div>;
-};
-
-export default Index;
---
+++ packages/ui/src/App.tsx
@@ -0,0 +1,5 @@
+import * as styles from "./App.module.css";
+
+export default function App() {
+  return <div className={styles.app}>Hello World</div>;
+}
