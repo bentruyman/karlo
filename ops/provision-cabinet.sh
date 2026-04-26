796605ebf759a0edafc5834fe468d35133b4f6f5
Ben Truyman <ben@truyman.com>
2026-04-25 20:42:55 -0500

    fix: install GStreamer video playback packages

--- ops/provision-cabinet.sh
+++ ops/provision-cabinet.sh
@@ -44,7 +44,10 @@ run env DEBIAN_FRONTEND=noninteractive apt-get install -y \
   gstreamer1.0-libav \
   gstreamer1.0-plugins-bad \
   gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-base-apps \
   gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-ugly \
+  gstreamer1.0-tools \
   libayatana-appindicator3-1 \
   librsvg2-2 \
   libwebkit2gtk-4.1-0 \
